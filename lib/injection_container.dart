c787b90ed754142dd0f176c338c92c0c0b0a437c
johnson-oragui <johnson.oragui@gmail.com>
2025-12-16 15:20:10 +0100

    feat: added initializeDependencies

---
+++ lib/injection_container.dart
@@ -0,0 +1,38 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'features/auth/data/datasources/auth_remote_data_source.dart';
+import 'features/auth/data/repositories/auth_repository_impl.dart';
+import 'features/auth/domain/repositories/auth_repository.dart';
+import 'features/auth/domain/usecases/signin_usecase.dart';
+import 'features/auth/domain/usecases/signup_usecase.dart';
+import 'features/auth/presentation/bloc/auth_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  // External
+  sl.registerLazySingleton(() => http.Client());
+
+  // Data Sources
+  sl.registerLazySingleton<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(client: sl()),
+  );
+
+  // Repositories
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(remoteDataSource: sl()),
+  );
+
+  // Use Cases
+  sl.registerLazySingleton(() => SignInUseCase(sl()));
+  sl.registerLazySingleton(() => SignUpUseCase(sl()));
+
+  // Bloc
+  sl.registerFactory(
+    () => AuthBloc(
+      signInUseCase: sl(),
+      signUpUseCase: sl(),
+      authRepository: sl(),
+    ),
+  );
+}
